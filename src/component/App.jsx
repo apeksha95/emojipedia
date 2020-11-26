22f2244ce3f4f2f0288719c5442f64a690240e24
Apeksha Kale <53891161+apeksha95@users.noreply.github.com>
2020-11-26 15:02:16 +0530

    Initial commit

---
+++ src/component/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Entry from "./Entry";
+import Emojipidea from "../emojipidea";
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1 className="heading">
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{Emojipidea.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
